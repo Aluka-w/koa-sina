d941b14e00073128e7144c0e76c14df152ba8a65
Aluka-w <1103500835@qq.com>
2020-04-28 11:08:38 +0800

    关注和取消关注测试用例

--- test/blog/profile.test.js
+++ test/blog/profile.test.js
@@ -2,11 +2,11 @@
  * @description 个人主页 test
  */
 const server = require('../server')
-const { Z_COOKIE, USER_NAME } = require('../testUserInfo')
+const { Z_COOKIE, Z_USER_NAME } = require('../testUserInfo')
 
 test('个人主页，加载第一页数据，应该成功', async () => {
   const res = await server
-    .get(`/api/profile/loadMore/${USER_NAME}/0`)
+    .get(`/api/profile/loadMore/${Z_USER_NAME}/0`)
     .set('cookie', Z_COOKIE)
   expect(res.body.errno).toBe(0)
 
--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -2,6 +2,11 @@
  * 存储的测试用户信息
  */
 module.exports = {
-  USER_NAME: "zhangsan",
-  Z_COOKIE: 'weibo.sid=C8QwHBkg8rbcFD3C7tJmnXsdRTNMY4yF; weibo.sid.sig=KDHcKuNvKsGVCPi8FB6wkpFEz6M'
+  Z_ID: 1,
+  Z_USER_NAME: 'zhangsan',
+  Z_COOKIE: 'weibo.sid=fPtIKHBEgJh8QJmh_4E0DfsTHI55dMid; weibo.sid.sig=JqjZxckwq3ymZLr7hlWdbhUxKFs',
+
+  L_ID: 2,
+  L_USER_NAME: 'lisi',
+  // L_COOKIE: 'weibo.sid=686m0u92zMTCmmSQY85trYv_PScDgpFx; weibo.sid.sig=Za_Oj-wSLFrG5QzQhqQU0YQxOFo'
 }
